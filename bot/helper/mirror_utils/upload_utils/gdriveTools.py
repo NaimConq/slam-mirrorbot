92410a74a6d9403cd5a24dc1758f1135006a49e7
NaimConq <86662931+NaimConq@users.noreply.github.com>
2021-07-01 02:16:52 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -539,9 +539,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirror Bot Search',
-                                 author_name='Slam Mirror Bot',
-                                 author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                 title = 'PrimeCheat Mirror Bot',
+                                 author_name='ＰＲＩＭＥ乛 🄰🄺🄰🅂🄷🇧🇩',
+                                 author_url='https://t.me/PrimeAkash',
                                  html_content=content)
         return
 
@@ -624,9 +624,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirror Bot Search',
-                                                        author_name='Slam Mirror Bot',
-                                                        author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                                        title = 'PrimeCheat Mirror Bot',
+                                                        author_name='ＰＲＩＭＥ乛 🄰🄺🄰🅂🄷🇧🇩',
+                                                        author_url='https://t.me/PrimeAkash',
                                                         html_content=content
                                                         )['path'])
 
